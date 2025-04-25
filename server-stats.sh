6e3aa81f9d7865decd0e751872eedecaab6912d3
pradipmengal <97036198+pradipmengal@users.noreply.github.com>
2025-04-25 13:36:21 +0530

    Update server-stats.sh

--- server-stats.sh
+++ server-stats.sh
@@ -10,3 +10,6 @@ free | grep "Mem" | awk '{print "Used Memory :"  $3 "\nFREE : " $4}'
 
 echo "===== Disk Usages ======"
 df -h | grep "/dev/sda1" | awk '{ print "Free :" $4 "\nUsed :" $3}'
+
+echo "===== CPU Processes ====="
+ps aux --sort=-%cpu | head -n 6 | awk '{print "Process " $11}'
